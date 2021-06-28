4583222bda920093d2588f35f1b225687ff3745b
awrde <awrde26@gmail.com>
2021-06-28 16:39:36 +0900

    [add] routes '/post'

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const postsRouter = require('./routes/posts')
 const app = express()
 
 app.set('view engine', 'ejs')
+
+app.use('/posts', postsRouter)
 
 app.get('/', (req, res) => {
     res.render('index')
